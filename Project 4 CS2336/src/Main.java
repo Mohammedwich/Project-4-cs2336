41388cb485789554309d2c6955567bcd9c7076a5
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2020-04-17 18:18:46 -0500

    Wrote functions: One checks path validity and one returns path weight.

--- Project 4 CS2336/src/Main.java
+++ Project 4 CS2336/src/Main.java
@@ -2,6 +2,8 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.ListIterator;
 import java.util.Scanner;
@@ -13,7 +15,7 @@ import java.util.Scanner;
 public class Main
 {
 
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args) throws IOException
 	{
 		String galaxyFileName;
 		String routesFileName;
@@ -30,13 +32,24 @@ public class Main
 		
 		if(galaxyFile.exists() == false)
 		{
-			System.out.println("The file " + galaxyFileName + " does not exist.");
+			System.out.println("The file " + galaxyFileName + " does not exist. Program exiting...");
 			inputReader.close();
 			return;
 		}
 		if(routesFile.exists() == false)
 		{
-			System.out.println("The file " + routesFileName + " does not exist.");
+			System.out.println("The file " + routesFileName + " does not exist. Program exiting...");
+			inputReader.close();
+			return;
+		}
+		
+		File outputFile = new File("patrols.txt");
+		
+		outputFile.createNewFile();
+		
+		if(outputFile.exists() == false)
+		{
+			System.out.println("The output file  patrols.txt  was not created. Program exiting...");
 			inputReader.close();
 			return;
 		}
@@ -46,6 +59,7 @@ public class Main
 		Scanner planetsReader = new Scanner(galaxyFile); // used to collect planet names (first word of each line)
 		Scanner galaxyReader = new Scanner(galaxyFile); // used for reading the whole file and processing
 		Scanner routesReader = new Scanner(routesFile);
+		FileWriter outputWriter = new FileWriter(outputFile);
 		
 		//Will hold galaxy names so we can use their indices to identify them. 
 		//These indices will be used as the index for the respective planet in the 2D adjacency matrix for the planets
@@ -104,10 +118,85 @@ public class Main
 		
 		
 		
+		
+		
 		inputReader.close();
 		planetsReader.close();
 		galaxyReader.close();
 		routesReader.close();
+	} // main end
+	
+//*********************************************************************************8
+	public static boolean checkPathValidity(ArrayList<ArrayList<Integer>> theMatrix, ArrayList<String> planetsList, ArrayList<String> thePath)
+	{
+		// deal with edge cases
+		if(thePath.size() == 0)
+		{
+			return false;
+		}
+		if(thePath.size() == 1)
+		{
+			String singlePlanet = thePath.get(0);
+			
+			if(planetsList.contains(singlePlanet) == true)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		//---------------------------------------------
+		
+		boolean result = true;
+		int pathSize = thePath.size();
+		
+		for(int i = 1; i < pathSize; ++i)
+		{
+			String currentPlanet = thePath.get(i-1);
+			String nextPlanet = thePath.get(i);
+			
+			int currentPlanetIndex = planetsList.indexOf(currentPlanet);
+			int nextPlanetIndex = planetsList.indexOf(nextPlanet);
+			
+			int edgeWeight = theMatrix.get(currentPlanetIndex).get(nextPlanetIndex);
+			
+			if(edgeWeight == 0) // if there is no edge between the two, the path is invalid and we return false
+			{
+				result = false;
+				break;
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	public static int getPathWeight(ArrayList<ArrayList<Integer>> theMatrix, ArrayList<String> planetsList, ArrayList<String> thePath)
+	{
+		if( checkPathValidity(theMatrix, planetsList, thePath) == false )
+		{
+			return 0;
+		}
+		
+		int result = 0;
+		int pathSize = thePath.size();
+		
+		for(int i = 1; i < pathSize; ++i)
+		{
+			String currentPlanet = thePath.get(i-1);
+			String nextPlanet = thePath.get(i);
+			
+			int currentPlanetIndex = planetsList.indexOf(currentPlanet);
+			int nextPlanetIndex = planetsList.indexOf(nextPlanet);
+			
+			int edgeWeight = theMatrix.get(currentPlanetIndex).get(nextPlanetIndex);
+			
+			result += edgeWeight;			
+		}
+		
+		return result;		
 	}
 
 }
