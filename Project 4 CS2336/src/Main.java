cc0eb81caab4843929fe3d328f540f388792e201
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2020-04-17 17:45:38 -0500

    Bugfix: Adjacency matrix is being created properly now

--- Project 4 CS2336/src/Main.java
+++ Project 4 CS2336/src/Main.java
@@ -43,7 +43,8 @@ public class Main
 		
 		//-----------------------------------------------------------
 		
-		Scanner galaxyReader = new Scanner(galaxyFile);
+		Scanner planetsReader = new Scanner(galaxyFile); // used to collect planet names (first word of each line)
+		Scanner galaxyReader = new Scanner(galaxyFile); // used for reading the whole file and processing
 		Scanner routesReader = new Scanner(routesFile);
 		
 		//Will hold galaxy names so we can use their indices to identify them. 
@@ -51,38 +52,32 @@ public class Main
 		ArrayList<String> galaxyList = new ArrayList<String>(); 
 		
 		//add all planet names to the list
-		while(galaxyReader.hasNextLine())
+		while(planetsReader.hasNextLine())
 		{
-			String currentLine = galaxyReader.nextLine();
+			String currentLine = planetsReader.nextLine();
 			Scanner lineReader = new Scanner(currentLine);
 			
 			galaxyList.add(lineReader.next());
 			lineReader.close();
 		}
 		
-		galaxyReader.reset(); //reset the scanner so we can process the whole file now
+		planetsReader.close();
+		
 		
 		//Each planet will have a row of its own represented by its index from galaxyNamesList. Each row will contain an integer
 	// representing the distance between the planet of that row and the planet represented by the index of the column this integer is in
 		ArrayList<ArrayList<Integer>> adjacencyMatrix = new ArrayList<ArrayList<Integer>>();
 		
-		//Fill the list so we have an empty(zero initialized) Size x Size matrix so they exist so we can reference those indices later
-		for(int i = galaxyList.size(); i > 0; --i)
-		{
-			ArrayList<Integer> theList = new ArrayList<Integer>(galaxyList.size()); 
-			for(int j = 0; j < galaxyList.size(); ++j)
-			{
-				theList.add(0);
-			}
-			
-			adjacencyMatrix.add(theList);
-		}
-		
-			
-		int currentRowIndex = 0;
-		
 		while(galaxyReader.hasNextLine())
 		{
+			//This list will be added to the array after weights are placed in the appropriate positions
+			ArrayList<Integer> neighborsList = new ArrayList<Integer>(galaxyList.size()); 
+			for(int j = 0; j < galaxyList.size(); ++j)
+			{
+				neighborsList.add(0);
+			}
+			
+			
 			String currentLine = galaxyReader.nextLine();
 			currentLine = currentLine.replace(",", " "); //replace all commas with space so we can pick the planet and the weight individually
 			Scanner lineReader = new Scanner(currentLine);
@@ -97,8 +92,10 @@ public class Main
 				
 				int planetIndex = galaxyList.indexOf(planetName); //The neighbor planet
 				
-				adjacencyMatrix.get(currentRowIndex).set(planetIndex, weight); 
+				neighborsList.set(planetIndex, weight); 
 			}
+			
+			adjacencyMatrix.add(neighborsList);
 			
 			lineReader.close();
 		}
@@ -106,7 +103,9 @@ public class Main
 		
 		
 		
+		
 		inputReader.close();
+		planetsReader.close();
 		galaxyReader.close();
 		routesReader.close();
 	}
